a6bd255f1c98b9a4737906aaddbe34661f0648e6
Kenan Yildirim <kenan@kenany.me>
2017-01-11 12:38:22 -0500

    implement StreamGrid component

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,8 @@
-import * as React from 'react';
-import { Route, Redirect, IndexRedirect, IndexRoute } from 'react-router';
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
 
 import Strims from './components/Strims';
 import Error404 from './components/Error404';
-
 
 const routes =
   <Route path='/'>
