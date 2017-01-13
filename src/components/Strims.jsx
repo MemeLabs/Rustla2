e71e0a179cd0e3b1fa73b598163aecf0711dd914
Kenan Yildirim <kenan@kenany.me>
2017-01-13 11:07:51 -0500

    fix lint issues

--- src/components/Strims.jsx
+++ src/components/Strims.jsx
@@ -11,33 +11,37 @@ const Strims = props => {
   let grid = null;
   if (streams.length) {
     grid = streams.map(stream => {
-      return <div className='col-xs-12 col-sm-4 col-md-3 col-lg-2'>
-        <StreamThumbnail name = {stream.channel}
-          thumbnail = {stream.image_url} url = {stream.url}
-          service = {stream.platform} viewers = {stream.rustlers}
-          key = {stream.url}
-        />
-      </div>;
+      return (
+        <div className='col-xs-12 col-sm-4 col-md-3 col-lg-2' key={stream.url}>
+          <StreamThumbnail
+            name={stream.channel} thumbnail={stream.image_url} url={stream.url}
+            service={stream.platform} viewers={stream.rustlers}
+                                      />
+        </div>
+      );
     });
   }
 
-  return <MainLayout>
-    <h1 className='streams-headling'>See what {streams.reduce((sum, stream) => {
-      return sum + stream.rustlers;
-    }, 0)} rustlers are watching!</h1>
-    <div className='streams'>
-      {grid}
-    </div>
-  </MainLayout>;
+  return (
+    <MainLayout>
+      <h1 className='streams-headling'>See what {
+        streams.reduce((sum, stream) => sum + stream.rustlers, 0) } rustlers
+        are watching!
+      </h1>
+      <div className='streams'>
+        {grid}
+      </div>
+    </MainLayout>
+  );
 };
 
 Strims.propTypes = {
-  streams: React.PropTypes.array.isRequired
+  streams: React.PropTypes.array.isRequired,
 };
 
 function stateToProps(state) {
   return {
-    streams: state.streams
+    streams: state.streams,
   };
 }
 
