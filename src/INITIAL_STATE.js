7d2bb88635c3513c40a662c144362a8f3acbaaf1
blushies <blushies@users.noreply.github.com>
2017-01-17 10:01:55 -0800

    some progress

--- src/INITIAL_STATE.js
+++ src/INITIAL_STATE.js
@@ -1,3 +1,4 @@
 export default {
-  streams: [],
+  stream: null,
+  streams: {},
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,50 @@
-import { combineReducers } from 'redux';
+import INITIAL_STATE from '../INITIAL_STATE';
+import { actions as websocket_actions } from '../actions/websocket';
 
-import streams from './streams';
 
-const rootReducer = combineReducers({
-  streams,
-});
+function rootReducer(state, action) {
+  if (!state) {
+    return INITIAL_STATE;
+  }
+  switch (action.type) {
+    case websocket_actions.STREAM_SET: {
+      const [ stream ] = action.payload;
+      return {
+        ...state,
+        stream: stream.id,
+        streams: {
+          ...state.streams,
+          [stream.id]: stream,
+        },
+      };
+    }
+    case websocket_actions.RUSTLERS_SET: {
+      const [ id, rustlers ] = action.payload;
+      return {
+        ...state,
+        streams: {
+          ...state.streams,
+          [id]: {
+            ...state.streams[id],
+            rustlers,
+          },
+        },
+      };
+    }
+    case websocket_actions.STREAMS_SET: {
+      const [ streams ] = action.payload;
+      return {
+        ...state,
+        streams: {
+          ...state.streams,
+          ...streams.reduce((acc, stream) => {
+            acc[stream.id] = stream;
+          }, {}),
+        },
+      };
+    }
+  }
+  return state;
+}
 
 export default rootReducer;
