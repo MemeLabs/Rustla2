6fb51718eca1849e34b872018186d2df64e5255f
Kenan Yildirim <kenan@kenany.me>
2017-05-08 14:03:09 -0400

    redux-logger@^3.0.1

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@
 
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 
 import reducer from './reducers';
 import INITIAL_STATE from './INITIAL_STATE';
