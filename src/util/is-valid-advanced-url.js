869b234bb0265f48c6956b846b1a70b0e2ed05c3
Kenan Yildirim <kenan@kenany.me>
2017-07-18 15:57:06 -0400

    implement advanced streams
    Closes https://github.com/ILiedAboutCake/Rustla2/issues/29

---
+++ src/util/is-valid-advanced-url.js
@@ -0,0 +1,36 @@
+/**
+ * Checks whether or not `url` is a valid URL for an advanced stream. A valid
+ * advanced stream URL is one which satisfies all of the following conditions:
+ *
+ *   1. The URL is a valid URL, as defined by the WHATWG URL Standard.
+ *   2. The protocol of the URL is either "http:" or "https:".
+ *
+ * @param {URL} URLimpl Implementation of the `URL` class.
+ * @param {string} url The URL to inspect.
+ * @returns {boolean} Returns `true` if `url` is a valid URL for an advanced
+ *  stream, else `false`.
+ */
+function isValidAdvancedUrl(URLimpl, url) {
+  try {
+    return ['http:', 'https:'].includes(new URLimpl(url).protocol);
+  }
+  // Catch the `TypeError` which indicates that `url` is not a valid URL.
+  catch (e) {
+    return false;
+  }
+}
+
+/**
+ * Returns a version of the `isValidAdvancedUrl` function which is bounded to
+ * the given `URL` implementation.
+ *
+ * @param {URL} URLimpl Implementation of the `URL` class.
+ * @returns {Function} Returns `isValidAdvancedUrl` bounded to `URLimpl`.
+ */
+function createIsValidAdvancedUrl(URLimpl) {
+  return isValidAdvancedUrl.bind(null, URLimpl);
+}
+
+// Not using `export default` here because the rest of the codebase will be
+// `require()`ing this module instead of `import`ing it.
+module.exports = createIsValidAdvancedUrl;
