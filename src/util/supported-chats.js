005f8b6e0d7b93761f3d3b9415a97b8dc9280d10
Kenan Yildirim <kenan@kenany.me>
2018-04-19 14:25:15 -0400

    update to new twitch chat embed URL
    Closes https://github.com/MemeLabs/Rustla2/issues/50

--- src/util/supported-chats.js
+++ src/util/supported-chats.js
@@ -3,8 +3,8 @@
 const supportedChats = {
   'azubu': channel => `https://www.azubu.tv/${channel}/chatpopup`,
   'hitbox': channel => `https://www.hitbox.tv/embedchat/${channel}`,
-  'twitch-vod': channel => `https://www.twitch.tv/${channel}/chat?popout=`,
-  'twitch': channel => `https://www.twitch.tv/${channel}/chat?popout=`,
+  'twitch-vod': channel => `https://www.twitch.tv/embed/${channel}/chat`,
+  'twitch': channel => `https://www.twitch.tv/embed/${channel}/chat`,
   'ustream': channel => `https://www.ustream.tv/socialstream/${channel}`,
   'vaughn': channel => `https://vaughnlive.tv/popout/chat/${channel}`,
   'youtube': channel => `https://gaming.youtube.com/live_chat?v=${channel}&output=embed&embed_domain=${process.env.NODE_ENV === 'production' ? 'overrustle.com' : 'localhost'}`,
