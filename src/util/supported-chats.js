f9a2d2defee2dd9fe0ef111ccb0c0d23b3ef77cf
jbpratt <jbpratt78@gmail.com>
2018-09-21 13:46:25 -0500

    removed ustream chat

--- src/util/supported-chats.js
+++ src/util/supported-chats.js
@@ -6,7 +6,6 @@ const supportedChats: {
 } = {
   'twitch-vod': channel => `https://www.twitch.tv/embed/${channel}/chat?darkpopout`,
   'twitch': channel => `https://www.twitch.tv/embed/${channel}/chat?darkpopout`,
-  'ustream': channel => `https://www.ustream.tv/socialstream/${channel}`,
   'vaughn': channel => `https://vaughnlive.tv/popout/chat/${channel}`,
   'youtube': channel => `https://youtube.com/live_chat?v=${channel}&embed_domain=${process.env.NODE_ENV === 'production' ? 'strims.gg' : 'localhost'}`,
 };
