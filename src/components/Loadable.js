730947d74abedc7343ae897362d205ac8139863e
Kenan Yildirim <kenan@kenany.me>
2018-09-23 11:30:27 -0400

    add flow annotations to Loadable

--- src/components/Loadable.js
+++ src/components/Loadable.js
@@ -1,16 +1,19 @@
+// @flow
+
 import loadable from 'react-loadable';
+import type { MapOptions, Options } from 'react-loadable';
 
 import Loading from './Loading';
 
 
-const Loadable = (opts) =>
+const Loadable = (opts: Options<*, *>) =>
   loadable({
     loading: Loading,
     ...opts,
   })
   ;
 
-const LoadableMap = (opts) =>
+const LoadableMap = (opts: MapOptions<*, *>) =>
   loadable.Map({
     loading: Loading,
     ...opts,
