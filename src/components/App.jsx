244ca04ddbf82c40851e695ecf16a4534116e74a
Kenan Yildirim <kenan@kenany.me>
2018-09-23 12:16:57 -0400

    add prop types to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect, Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import { compose } from 'redux';
-import lifecycle from 'recompose/lifecycle';
+import { lifecycle } from 'recompose';
 
 import { login } from '../actions';
 import Routes from '../routes';
 import history from '../history';
 
-
-const App = ({ store }) => (
+const App = ({ store }) =>
   <Provider store={store}>
     <Router history={history}>
       <Routes />
     </Router>
-  </Provider>
-);
+  </Provider>;
+
+App.propTypes = {
+  store: PropTypes.object.isRequired,
+};
 
 export default compose(
   connect(
