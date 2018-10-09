1dfc9ee8a53d3e1b2b7038a23c13486a640e7e56
Kenan Yildirim <kenan@kenany.me>
2018-10-09 19:53:50 -0400

    add flow to Logout

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,15 +1,18 @@
+// @flow
+
 import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router';
 import lifecycle from 'recompose/lifecycle';
 import { compose } from 'redux';
+import type { Dispatch } from 'redux';
 
 import { logout } from '../actions';
 
 
 const Logout = () => <Redirect to='/' />;
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch: Dispatch<*>) {
   return {
     logout() {
       dispatch(logout());
