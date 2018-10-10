a0062706a5af86e7c414b43551643ecb9f7614ab
tensei c <davuttensei@gmail.com>
2018-10-10 05:23:34 -0700

    fixed youtube live chat embed (hopefully) #90

--- src/util/supported-chats.js
+++ src/util/supported-chats.js
@@ -6,7 +6,7 @@ const supportedChats: { [key: string]: (channel: string) => string } = {
   'twitch': channel => `https://www.twitch.tv/embed/${channel}/chat?darkpopout`,
   'ustream': channel => `https://www.ustream.tv/socialstream/${channel}`,
   'vaughn': channel => `https://vaughnlive.tv/popout/chat/${channel}`,
-  'youtube': channel => `https://gaming.youtube.com/live_chat?v=${channel}&output=embed&embed_domain=${process.env.NODE_ENV === 'production' ? 'strims.gg' : 'localhost'}`,
+  'youtube': channel => `https://youtube.com/live_chat?v=${channel}&embed_domain=${process.env.NODE_ENV === 'production' ? 'strims.gg' : 'localhost'}`,
 };
 
 export default supportedChats;
