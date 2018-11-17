46d5c244cfccf11b757f3eefd035535be51608d1
Kenan Yildirim <kenan@kenany.me>
2018-11-17 12:59:24 -0500

    enable flow in Banned

--- src/components/Banned.jsx
+++ src/components/Banned.jsx
@@ -1,20 +1,20 @@
+// @flow
+
 import React from 'react';
-import PropTypes from 'prop-types';
+import type { BrowserHistory } from 'history/createBrowserHistory';
 
 import MainLayout from './MainLayout';
 
+type Props = {
+  history: BrowserHistory
+};
 
-const Banned = ({ history }) =>
+const Banned = ({ history }: Props) =>
   <MainLayout history={history}>
     <div className='text-center'>
       <h1>Stream Banned</h1>
       <img src='/image/beand.jpg' className='img-top-margin' />
     </div>
-  </MainLayout>
-  ;
-
-Banned.propTypes = {
-  history: PropTypes.object.isRequired,
-};
+  </MainLayout>;
 
 export default Banned;
