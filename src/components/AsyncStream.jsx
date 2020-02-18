2b52939895570f15a6a6b6704e4be899b954ca1d
Slugalisk <slugalisk@gmail.com>
2020-02-17 23:43:01 -0800

    add logging for connection and stream open and close events

--- src/components/AsyncStream.jsx
+++ src/components/AsyncStream.jsx
@@ -34,47 +34,55 @@ type Props = {
   }
 };
 
-const AsyncStream = ({
-  history,
-  match: { params: { service, channel, streamer } },
-}: Props) => {
-  const LoadableStream = Loadable.Map<*, Modules>({
-    loader: {
-      Component: () => import(/* webpackChunkName: "stream" */ './Stream'),
-      streamer: () => {
-        // If this is the `/:service/:channel` route, then we do not need to
-        // fetch any addition information from the server.
-        if (service && channel) {
-          return Promise.resolve();
+class AsyncStream extends React.Component<Props> {
+  LoadableStream: React$ComponentType<any>;
+
+  constructor({
+    history,
+    match: { params: { service, channel, streamer } },
+  }: Props) {
+    super();
+
+    this.LoadableStream = Loadable.Map<*, Modules>({
+      loader: {
+        Component: () => import(/* webpackChunkName: "stream" */ './Stream'),
+        streamer: () => {
+          // If this is the `/:service/:channel` route, then we do not need to
+          // fetch any addition information from the server.
+          if (service && channel) {
+            return Promise.resolve();
+          }
+
+          // Otherwise, we're at the `/:streamer` route, and we need to fetch the
+          // service and channel for this streamer.
+          return fetch(`${API}/streamer/${streamer}`).then(res => res.json());
+        },
+      },
+      render(loaded) {
+        // If no service was provided, and none was found from the streamer
+        // lookup, then render the 404 page.
+        if (!service && !loaded.streamer.service) {
+          return <Error404 history={history} />;
         }
 
-        // Otherwise, we're at the `/:streamer` route, and we need to fetch the
-        // service and channel for this streamer.
-        return fetch(`${API}/streamer/${streamer}`).then(res => res.json());
+        const Component = loaded.Component.default;
+        return (
+          <Component
+            streamer={streamer}
+            service={service || loaded.streamer.service}
+            channel={channel || loaded.streamer.channel}
+            history={history}
+            />
+        );
       },
-    },
-    render(loaded) {
-      // If no service was provided, and none was found from the streamer
-      // lookup, then render the 404 page.
-      if (!service && !loaded.streamer.service) {
-        return <Error404 history={history} />;
-      }
+    });
+  }
 
-      const Component = loaded.Component.default;
-      return (
-        <Component
-          streamer={streamer}
-          service={service || loaded.streamer.service}
-          channel={channel || loaded.streamer.channel}
-          history={history}
-          />
-      );
-    },
-  });
-
-  // $FlowFixMe: lack of props here doesn't work with `TProps` being `empty`.
-  return <LoadableStream />;
-};
+  render(): React$Element<any> {
+    const {LoadableStream} = this;
+    return <LoadableStream />;
+  }
+}
 
 
 export default compose(
