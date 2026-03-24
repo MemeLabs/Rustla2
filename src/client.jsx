1283b78858e31a49157694807ed2b061e662cfff
ftwpala <ftwpala@gmail.com>
2026-03-24 04:32:23 +0100

    Green color on copy click, exposed some store functions to browser console for easier testing

--- src/client.jsx
+++ src/client.jsx
@@ -4,8 +4,20 @@ import { render } from 'react-dom';
 import App from './components/App';
 import store from './store';
 
-
 const mountPoint = document.getElementById('main');
+
+if (process.env.NODE_ENV !== 'production') {
+  // used for testing redux dispatch events in console, since we're using symbols
+  // they need to be attached to the window for them to match
+  // otherwise it goes into a catchall(default)
+  // you can now dispatch events using similar structure to line below
+  // window.store.dispatch({type:window.types.IMAGE_MODAL_SRC, payload:"url"})
+  import("./actions/index").then((types) => {
+    window.store = store; 
+    window.types = types;
+  });
+}
+
 render(
   <App store={store} />,
   mountPoint,
